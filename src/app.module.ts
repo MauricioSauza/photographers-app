ed155c241c205cd6062ebd36a09c94e945877056
Mauricio Sauza <mauriciosauzatorrez666@gmail.com>
2024-04-06 18:13:14 -0400

    Fixing database conection and refactoring database configuration into a module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,26 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-    envFilePath: ['.env']
-    }),
-    TypeOrmModule.forRootAsync({
-      useFactory: async () => ({
-        type: 'postgres',
-        host: 'postgres',
-        port: parseInt(process.env.POSTGRES_PORT || '5432'),
-        username: process.env.POSTGRES_USER,
-        password: process.env.POSTGRES_PASSWORD,
-        database: process.env.POSTGRES_DB,
-        autoLoadEntities: true,
-      })
-    }),
+    ConfigModule.forRoot({ isGlobal: true }),
     UserModule,
+    DatabaseModule,
 ],
   controllers: [AppController],
   providers: [AppService],
